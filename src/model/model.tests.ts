9bd710b2db947fc2b2574943cfaf76ede1149a12
cschleiden <cschleiden@live.de>
2016-10-01 11:59:54 -0700

    Basic html to md conversion

--- src/model/model.tests.ts
+++ src/model/model.tests.ts
@@ -4,14 +4,8 @@ import * as $ from "jquery";
 
 import { Model, Utils } from "./model";
 
-describe("Model", () => {
-    describe("getItem", () => {
-        let m: Model;
-
-        beforeEach(() => {
-            m = new Model();
-        });
-
+describe("Utils", () => {
+    describe("parse input", () => {
         it("should identify md content", () => {
             let result = Utils.extractMarkdown(`<p style="display:none;" id="md">md**test**</p>  asome&nbsp;<b>html&nbsp;</b>&nbsp;conest<div>asfdl long</div>`);
 
@@ -26,4 +20,30 @@ describe("Model", () => {
             expect(result.htmlContent).to.be.eq("<p>test</p>  asome&nbsp;<b>html&nbsp;</b>&nbsp;conest<div>asfdl long</div>");
         });
     });
+
+    describe("parse html", () => {
+        it("should parse common TFS formatting", () => {
+            check(
+                `<b>bold</b><div><i>italic</i></div><div><u>underline</u></div><div><b><i>bold-italic</i></b></div><div><b><u>bold-underline</u></b></div><div><u><i>italic-underline</i></u></div>`,
+                "**bold**\n_italic_\n<u>underline</u>\n**_bold-italic_**\n**<u>bold-underline</u>**\n<u>_italic-underline_</u>");
+        })
+
+        it("should parse spans", () => {
+            check(
+                `<span style="font-style: italic;">asome </span><b>html </b>&nbsp;con<span style="font-style: italic;">est</span>&nbsp;`,
+                "_asome_ **html** con_est_");
+
+        });
+
+        it("should parse images", () => {
+            check(
+                `<span style="font-style:italic;">test</span><div><img src="https://cs-brazil.visualstudio.com/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=67ab2c59-0aae-47d7-9ef2-a2669ce47f7f&amp;FileName=rejected.png" style="width:437.75px;"></div><div>test2</div>`,
+                `_test_\n![](https://cs-brazil.visualstudio.com/WorkItemTracking/v1.0/AttachFileHandler.ashx?FileNameGuid=67ab2c59-0aae-47d7-9ef2-a2669ce47f7f&FileName=rejected.png)\ntest2`);
+        });
+    });
+
+    let check = (input, output) => {
+        const test = Utils.convertToMarkdown(input);
+        expect(test).to.equal(output);
+    };
 });
--- src/model/model.ts
+++ src/model/model.ts
@@ -21,7 +21,7 @@ export class Model {
         const { markdownContent, htmlContent } = Utils.extractMarkdown(rawInput);
         if (markdownContent) {
             const generatedMarkdownContenxt = marked(markdownContent, markedOptions);
-            if (htmlContent !== generatedMarkdownContenxt) {
+            if (htmlContent.trim() !== generatedMarkdownContenxt.trim()) {
                 // Content was changed in legacy editor...
                 this.mode = Mode.MarkdownModified;
                 this.markdownContent = markdownContent;
@@ -34,7 +34,7 @@ export class Model {
         } else {
             // No markdown content
             this.mode = Mode.Legacy;
-            this.markdownContent = toMarkdown(rawInput);
+            this.markdownContent = Utils.convertToMarkdown(rawInput);
             this.htmlContent = rawInput;
         }
 
@@ -59,13 +59,45 @@ export class Model {
     }
 
     private _triggerChange() {
-        for(let listener of this._listeners) {
+        for (let listener of this._listeners) {
             listener();
         }
     }
 }
 
 export namespace Utils {
+    export function convertToMarkdown(value: string): string {
+        // Work around strange spacing issues
+        value = value.replace("&nbsp;", " ");
+
+        return toMarkdown(value, {
+            converters: [
+                {
+                    filter: "&nbsp;",
+                    replacement: (innerHtml) => " "
+                },
+                {
+                    filter: ["span"],
+                    replacement: (innerHtml, node: HTMLSpanElement) => {
+                        if (node.style && node.style.fontStyle) {
+                            if (node.style.fontStyle === "italic") {
+                                return `_${innerHtml}_`;
+                            } else if (node.style.fontStyle === "italic") {
+                                return `*${innerHtml}*`;
+                            }
+                        }
+
+                        return innerHtml;
+                    }
+                },
+                {
+                    filter: ["div"],
+                    replacement: (innerHtml) => `\n${innerHtml}`
+                }
+            ]
+        });
+    }
+
     export function extractMarkdown(value: string): { markdownContent: string; htmlContent: string; } {
         let parsed = $("<div></div>").html(value);
         let mdElement = parsed.find("#md");
